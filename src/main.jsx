7fbcffa72620df0d9b9f3bfbfc186040893c07f9
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 22:34:01 -0300

    feat: basic main page

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import Provider from './context/Provider';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
