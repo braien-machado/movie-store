5ff1ad7e1158a6744980eb340600947ccf3a4877
Braien Machado de Paula <braienmp@outlook.com>
2022-07-05 11:36:06 -0300

    fix: hard code api_key

--- settings.js
+++
@@ -1,3 +0,0 @@
-const environment = {
-  API_KEY: 'ddbcbe0588b781fba568d59761e0c9fd',
-};
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,5 +1,7 @@
+const API_KEY = 'ddbcbe0588b781fba568d59761e0c9fd';
+
 async function getMovies(page = 1) {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}&language=pt&page=${page}`)
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt&page=${page}`)
     .then((result) => result.json());
 
   return data.errors !== undefined ? undefined : data;
