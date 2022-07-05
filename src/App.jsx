3b5cc2331dbc5508f73be343a4d8a45b863e29c9
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 22:44:19 -0300

    style: fix eslint issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import Router from './Router';
 
 function App() {
-
   return (
     <div className="App">
       <Router />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
