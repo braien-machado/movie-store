fd0d050653ba631cd1e9cf16f93815e6edc2e355
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 23:59:47 -0300

    chore: basic favorites page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Favorites from './pages/Favorites';
 import Home from './pages/Home';
-
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={ <Home /> } />
+      <Route path="/favorites" element={ <Favorites /> } />
     </Routes>
   );
 }
