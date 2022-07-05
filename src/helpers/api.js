614df3cad7645f9c11b5250cc9576bdd5746ccc0
Braien Machado de Paula <braienmp@outlook.com>
2022-07-05 11:05:16 -0300

    fix: handle api error

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ async function getMovies(page = 1) {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}&language=pt&page=${page}`)
     .then((result) => result.json());
 
-  return data;
+  return data.errors !== undefined ? undefined : data;
 }
 
 export default getMovies;
