1f4bdb3eebba52ef2fd9820037dc1d76d3903eb8
Braien Machado de Paula <braienmp@outlook.com>
2022-07-05 11:32:06 -0300

    fix: api requests and infinite scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,55 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import getMovies from '../helpers/api';
 import MovieCard from '../components/MovieCard';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const loaderRef = useRef(null);
 
-  // Infinite scroll implementation
-  useEffect(() => {
-    const options = {
-      root: null,
-      // rootMargin: '20px',
-      threshold: 1.0,
-    };
+  async function loadMovies() {
+    const nextPage = currentPage + 1;
+    const data = await getMovies(nextPage);
 
-    console.log('aqui');
-
-    const observer = new IntersectionObserver((entities) => {
-      const target = entities[0];
-
-      if (target.isIntersecting) {
-        setCurrentPage((old) => old + 1);
-      }
-    }, options);
-
-    if (loaderRef.current) {
-      observer.observe(loaderRef.current);
+    if (!data) {
+      return;
     }
-  }, []);
+    setMovies((old) => [...old, ...data.results]);
+    setCurrentPage(nextPage);
+  }
 
   useEffect(() => {
     async function fetchMovies() {
-      const data = await getMovies(currentPage);
-      console.log(data);
+      const data = await getMovies();
 
       if (!data) {
         return;
       }
-      setMovies((old) => [...old, ...data.results]);
+      setMovies(data.results);
     }
 
     fetchMovies();
-  }, [currentPage]);
+  }, []);
+
+  console.log(movies, currentPage);
 
   return (
     <div>
-      <div className="grid grid-cols-4">
+      <InfiniteScroll
+        className="grid grid-cols-4"
+        dataLength={ movies.length }
+        next={ () => loadMovies() }
+        hasMore
+        loader={ <h4 className="text-green-600">Carregando...</h4> }
+        endMessage={
+          <p style={ { textAlign: 'center' } }>
+            <b>Você chegou ao fim do catálogo!</b>
+          </p>
+        }
+      >
         { movies.map((movie, index) => (
           <MovieCard movie={ movie } key={ `${movie.id}-${index}` } />))}
-      </div>
-      {
-        movies.length > 0
-        && (
-          <p
-            className="py-4"
-            ref={ loaderRef }
-          >
-            Carregando mais episodios...
-          </p>)
-      }
+      </InfiniteScroll>
     </div>
   );
 }
