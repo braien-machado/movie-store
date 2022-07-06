01b3407a10b7fc16eb0f7b25a5b987f6998d9b0e
Braien Machado de Paula <braienmp@outlook.com>
2022-07-06 15:57:47 -0300

    fix: responsiveness

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,6 +3,11 @@ import CheckoutForm from '../components/CheckoutForm';
 import CheckoutTable from '../components/CheckoutTable';
 import CheckoutSubmit from '../components/CheckoutSubmit';
 
+const BASIC_STYLE = 'flex flex-col w-full items-center gap-8';
+const MEDIUM_STYLE = ' md:items-start md:flex-row md:justify-between';
+const LARGE_STYLE = ' xl:gap-0 2xl:justify-center 2xl:gap-[10%]';
+const COMPLETE_STYLE = BASIC_STYLE + MEDIUM_STYLE + LARGE_STYLE;
+
 export default function Checkout() {
   return (
     <main className="flex flex-col gap-8 items-center sm:items:start sm:mx-[10%]">
@@ -12,8 +17,7 @@ export default function Checkout() {
         Finalizar Compra
       </h1>
       <div
-        className={ `"flex flex-col w-full items-center md:items-start md:flex-row gap-8
-        xl:gap-0 md:justify-between 2xl:justify-center 2xl:gap-[10%]"` }
+        className={ COMPLETE_STYLE }
       >
         <CheckoutForm />
         <div className="flex flex-col justify-between gap-8 max-w-[500px]">
