5c6c7279d2bbb50a823a131210fa66efbd15a2f5
Braien Machado de Paula <braienmp@outlook.com>
2022-07-06 16:11:27 -0300

    fix: cart responsiveness

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,39 +20,41 @@ export default function Cart() {
     <div
       className={ `fixed top-16 right-0 z-10 h-[calc(100%-64px)] w-full
       sm:w-[500px] max-w-[500px] bg-gray-100 border-2 border-gray-400
-      flex flex-col gap-8 px-4 pt-4` }
+      flex flex-col justify-between px-4 pt-4 gap-8 overflow-y-auto no-scrollbar` }
     >
-      <ListHeader clear={ clearCart } list={ cart } title="Meu Carrinho" />
-      <div className="flex flex-col gap-2 h-[65%] overflow-y-auto no-scrollbar">
-        {
-          !cart
-            ? <span className="text-center">Seu carrinho está vazio.</span>
-            : (
-              cart.map((product) => (
-                <div
-                  key={ product.id }
-                  className="flex gap-4 h-8 justify-between items-center"
-                >
-                  <ListItemCoreInfo title={ product.title } image={ product.image } />
-                  <div className="flex items-center gap-12">
-                    <span className="w-4 flex justify-end">{ product.quantity }</span>
-                    <span
-                      className="w-10 flex justify-end"
-                    >
-                      { priceToReal(product.price * product.quantity) }
-                    </span>
-                    <button
-                      onClick={ () => removeProduct(product.id) }
-                      type="button"
-                      className="hover:text-gray-400 transition-colors"
-                    >
-                      <Trash size={ 20 } weight="fill" />
-                    </button>
+      <div className="flex flex-col gap-8 max-h-fit">
+        <ListHeader clear={ clearCart } list={ cart } title="Meu Carrinho" />
+        <div className="flex flex-col gap-2">
+          {
+            !cart
+              ? <span className="text-center">Seu carrinho está vazio.</span>
+              : (
+                cart.map((product) => (
+                  <div
+                    key={ product.id }
+                    className="flex gap-4 h-8 justify-between items-center"
+                  >
+                    <ListItemCoreInfo title={ product.title } image={ product.image } />
+                    <div className="flex items-center gap-12">
+                      <span className="w-4 flex justify-end">{ product.quantity }</span>
+                      <span
+                        className="w-10 flex justify-end"
+                      >
+                        { priceToReal(product.price * product.quantity) }
+                      </span>
+                      <button
+                        onClick={ () => removeProduct(product.id) }
+                        type="button"
+                        className="hover:text-gray-400 transition-colors"
+                      >
+                        <Trash size={ 20 } weight="fill" />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))
-            )
-        }
+                ))
+              )
+          }
+        </div>
       </div>
       <CartCheckout cart={ cart } />
     </div>
