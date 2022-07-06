50cfaedc24fdd7e0b61e92a2b55df51bcca980f8
Braien Machado de Paula <braienmp@outlook.com>
2022-07-06 17:39:46 -0300

    feat: add masks to inputs

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,33 +1,107 @@
 import React from 'react';
+import InputMask from 'react-input-mask';
+import PropTypes from 'prop-types';
 
 const INPUT_BASIC_STYLE = `border-2 bg-gray-100 border-gray-300 p-3 rounded
 focus:outline-gray-500`;
 
-export default function CheckoutForm() {
+export default function CheckoutForm(props) {
+  const { values, updateValue } = props;
+  const {
+    name,
+    cpf,
+    cel,
+    email,
+    cep,
+    address,
+    city,
+    state,
+  } = values;
+
+  const handleChange = (key, value) => updateValue({ ...values, [key]: value });
+
   return (
     <form className="flex flex-col gap-8 w-[90%] max-w-[492px] md:w-[300px] xl:w-[492px]">
       <input
         className={ `${INPUT_BASIC_STYLE}` }
         type="text"
         placeholder="Nome Completo"
+        value={ name }
+        onChange={ ({ target: { value } }) => handleChange('name', value) }
       />
       <div className="flex gap-8 flex-col sm:flex-row md:flex-col xl:flex-row">
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="CPF" />
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="Celular" />
+        <InputMask
+          mask="999.999.999-99"
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="CPF"
+          value={ cpf }
+          onChange={ ({ target: { value } }) => handleChange('cpf', value) }
+        />
+        <InputMask
+          mask="(99) 99999-9999"
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="Celular"
+          value={ cel }
+          onChange={ ({ target: { value } }) => handleChange('cel', value) }
+        />
       </div>
       <input
         className={ `${INPUT_BASIC_STYLE}` }
         type="email"
         placeholder="E-mail"
+        value={ email }
+        onChange={ ({ target: { value } }) => handleChange('email', value) }
       />
       <div className="flex gap-8 flex-col sm:flex-row md:flex-col xl:flex-row">
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="CEP" />
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="Endereço" />
+        {/* cep input width must be half of its sibling */}
+        <InputMask
+          mask="99999-999"
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="CEP"
+          value={ cep }
+          onChange={ ({ target: { value } }) => handleChange('cep', value) }
+        />
+        <input
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="Endereço"
+          value={ address }
+          onChange={ ({ target: { value } }) => handleChange('address', value) }
+        />
       </div>
       <div className="flex gap-8 flex-col sm:flex-row md:flex-col xl:flex-row">
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="Cidade" />
-        <input className={ `${INPUT_BASIC_STYLE}` } type="text" placeholder="Estado" />
+        <input
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="Cidade"
+          value={ city }
+          onChange={ ({ target: { value } }) => handleChange('city', value) }
+        />
+        <input
+          className={ `${INPUT_BASIC_STYLE}` }
+          type="text"
+          placeholder="Estado"
+          value={ state }
+          onChange={ ({ target: { value } }) => handleChange('state', value) }
+        />
       </div>
     </form>
   );
 }
+
+CheckoutForm.propTypes = {
+  updateValue: PropTypes.func.isRequired,
+  values: PropTypes.shape({
+    name: PropTypes.string,
+    cpf: PropTypes.string,
+    cel: PropTypes.string,
+    email: PropTypes.string,
+    cep: PropTypes.string,
+    address: PropTypes.string,
+    city: PropTypes.string,
+    state: PropTypes.string,
+  }).isRequired,
+};
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CheckoutForm from '../components/CheckoutForm';
 import CheckoutTable from '../components/CheckoutTable';
 import CheckoutSubmit from '../components/CheckoutSubmit';
@@ -9,6 +9,19 @@ const LARGE_STYLE = ' xl:gap-0 2xl:justify-center 2xl:gap-[10%]';
 const COMPLETE_STYLE = BASIC_STYLE + MEDIUM_STYLE + LARGE_STYLE;
 
 export default function Checkout() {
+  const [customerInfo, setCustomerInfo] = useState(
+    {
+      name: '',
+      cpf: '',
+      cel: '',
+      email: '',
+      cep: '',
+      address: '',
+      city: '',
+      state: '',
+    },
+  );
+
   return (
     <main className="flex flex-col gap-8 items-center sm:items:start sm:mx-[10%]">
       <h1
@@ -19,10 +32,10 @@ export default function Checkout() {
       <div
         className={ COMPLETE_STYLE }
       >
-        <CheckoutForm />
+        <CheckoutForm values={ customerInfo } updateValue={ setCustomerInfo } />
         <div className="flex flex-col justify-between gap-8 max-w-[500px]">
           <CheckoutTable />
-          <CheckoutSubmit />
+          <CheckoutSubmit values={ customerInfo } />
         </div>
       </div>
     </main>
